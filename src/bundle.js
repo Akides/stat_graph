2973ca52114669abe9ac781854f9f2917ca54f7a
Aki <uenoaki97@gmail.com>
2022-06-14 09:31:09 +0200

    change line width and delete points

--- src/bundle.js
+++ src/bundle.js
@@ -13398,25 +13398,15 @@ const data = {
       'Running'
     ],
     datasets: [{
-      label: 'My First dsadsdsasasasa',
+      label: 'Tea Stats',
       data: [65, 59, 90, 81, 56, 55, 40],
       fill: true,
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgb(255, 99, 132)',
-      pointBackgroundColor: 'rgb(255, 99, 132)',
-      pointBorderColor: '#fff',
+      backgroundColor: "#B9FFB9",   //content
+      borderColor: 'rgb(50, 255, 50)',  //lines
+      pointBackgroundColor: 'rgb(50, 255, 50)',
+      pointBorderColor: 'rgb(0, 255, 0)',
       pointHoverBackgroundColor: '#fff',
       pointHoverBorderColor: 'rgb(255, 99, 132)'
-    }, {
-      label: 'My Second Dataset',
-      data: [28, 48, 40, 19, 96, 27, 100],
-      fill: true,
-      backgroundColor: 'rgba(54, 162, 235, 0.2)',
-      borderColor: 'rgb(54, 162, 235)',
-      pointBackgroundColor: 'rgb(54, 162, 235)',
-      pointBorderColor: '#fff',
-      pointHoverBackgroundColor: '#fff',
-      pointHoverBorderColor: 'rgb(54, 162, 235)'
     }]
   };
 
@@ -13426,7 +13416,10 @@ const data = {
     options: {
       elements: {
         line: {
-          borderWidth: 3
+          borderWidth: 1
+        },
+        point: {
+          radius: 0
         }
       }
     },
