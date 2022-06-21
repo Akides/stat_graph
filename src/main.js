9ad37df3790e911c491b2d516815aa74e6449b2e
Aki <uenoaki97@gmail.com>
2022-06-21 18:33:55 +0200

    change visuals

--- src/main.js
+++ src/main.js
@@ -11,9 +11,8 @@ const data = {
       'Consistency'
     ],
     datasets: [{
-      label: 'Tea Stats',
       data: [1, 3.5, 4, 3, 1],
-      backgroundColor: "rgba(00, 255, 00, 0.1)",   //content
+      backgroundColor: 'rgba(200, 255, 200, 0.5)', //content
       borderColor: 'rgba(50, 255, 50)',  //lines
       pointBackgroundColor: 'rgb(00, 255, 00)',
       pointBorderColor: 'rgb(0, 255, 0)',
@@ -26,6 +25,15 @@ const data = {
     type: 'radar',
     data: data,
     options: {
+      plugins: {
+        legend: {
+          display: false
+        },
+      },
+      responsive: false,
+      maintainAspectRatio: true,
+      devicePixelRatio: 5,
+      
       elements: {
         line: {
           borderWidth: 1
@@ -37,6 +45,7 @@ const data = {
       scales: {
         r: {
           angleLines: {
+            display: false,
             color: backgroundColor
           },
           grid: {
@@ -45,16 +54,17 @@ const data = {
           min: 0,
           max: 5,
           ticks: {
+            display: false,
             beginAtZero: false,
             stepSize: 1
           }
         }
       }
-    },
+    }
   };
 
   // render chart
-  const myChart = new Chart(
+  const myChart = new chart(
     document.getElementById('graph'),
     config
   );
